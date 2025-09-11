34742031c781f2be9d9ae1b38bc1b97d72440a87
HenryWinNguyen <henrynguyen2394@gmail.com>
2025-09-11 18:05:48 -0500

    Add src/pages/index.tsx for Next build

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "ui-sans-serif, system-ui" }}>
+      <h1 style={{ fontSize: 28, marginBottom: 8 }}>NBA Muse</h1>
+      <p>Deployment check page. App UI will be added next.</p>
+    </main>
+  );
+}
