06bddf91a10b6816e1851b2d2c072e5bad260a4d
HenryWinNguyen <henrynguyen2394@gmail.com>
2025-09-12 15:26:58 -0500

    Serve static UI at /app.html; redirect / -> /app.html

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     async redirects() {
       // Send "/" to your static HTML so it looks EXACTLY like local testing
-      return [{ source: "/", destination: "/index.html", permanent: false }];
+      return [{ source: "/", destination: "/app.html", permanent: false }];
     },
   };
   module.exports = nextConfig;
